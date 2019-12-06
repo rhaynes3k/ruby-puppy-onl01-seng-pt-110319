b0a360cc4f8ee06fc0280f858b8797aa8e9eb5bb
Rommel Haynes <rhaynes3k@gmail.com>
2019-12-06 06:09:55 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+require 'pry'
+class Dog
+  @@all = []
+  @@names = []
+  def initialize(name)
+    @name = name
+    save
+    @@names << name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all.clear
+    @@names.clear
+  end
+  
+  def self.print_all
+    puts @@names.uniq
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def name
+    @name
+  end
+end
